264b7ac6a2657dfe4f794244363d7bd13cdb8a28
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-12-20 15:39:27 -0500

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
+
+def hello_t(array)
+ if block_given?
+   i = 0 
+
+ while i < array.length  
+    array[i]
+    yield(array[i])
+    i += 1
+ end
+  array
+
+else    
+    puts "Hey! No block was given!"
 
 end
 
-# call your method here!
 
+end
+
+#array.each {|num| num + 2}
